0d29b1b9d9b22c4e3ca529f1b5c4fe1779fbe091
IT24100307 Bandara I G C <166091696+ChandimaBandara815@users.noreply.github.com>
2025-02-25 02:46:02 +0530

    1st one

---
+++ Tutorial 1/Lab_1.py
@@ -0,0 +1,43 @@
+#1. Write a Python program to,
+#(a) Add 20 and 15, and print the result.
+print(20+15)
+#(b) Subtract 12 from 50, and print the result.
+print(12-50)
+#(c) Multiply 8 and 7, and print the result.
+print(8*7)
+#(d) Divide 100 by 25, and print the result.
+print(100/25)
+#(e) Find the remainder when 17 is divided by 5, and print the result.
+print(17%5)
+
+#2. Write a Python program that,
+#(a) Compares two numbers (x = 15, y = 10) and prints whether x is greater thany.
+x=15
+y=10
+print(x>y)
+#(b) Checks if 25 is equal to 25 and prints the result.
+print(25==25)
+#(c) Checks if 30 is not equal to 25 and prints the result.
+print(30!=25)
+#(d) Compares 10 and 20 using <= and prints the result.
+print(10<=20)
+
+#3. Write a Python program to
+#(a) Check if both 5 > 3 and 10 > 5 are true, and print the result
+print((5>3)and(10>5))
+#(b) Check if 7 > 9 or 15 > 10 is true, and print the result.
+print((7>9)or(15>10))
+#(c) Use not to check if 8 == 8 and print the result.
+print(not(8==8))
+
+#4. Define variables for the following data types and print them,
+#(a) An integer x = 100.
+x = 100
+print(x)
+#(b) A float y = 45.67
+y = 45.67
+print(y)
+#(c) A string name = ”Alice”.
+name = "Alice"
+print(name)
+
